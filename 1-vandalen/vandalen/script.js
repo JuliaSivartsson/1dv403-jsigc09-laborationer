757153cfd6436f7c2e027d078c1cd08c209dd8bb
juliasivartsson <jsigc09@student.lnu.se>
2014-11-07 19:00:59 +0000

    vandalen is working

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,34 @@
 
 var makePerson = function(persArr){
 
+    // New array
+    var result = {};
 
-	// Din kod här...
-
-}
-
+   
+    //Property minAge
+    persArr.sort(function(a, b){
+        return a.age-b.age;
+    });
+    result.minAge = persArr[0].age;
+    
+    //Property maxAge
+    var tempMaxAge = (persArr.length - 1);
+    result.maxAge = persArr[tempMaxAge].age;
+    
+    //Property averageAge
+    var arrayForNames = [];
+    var sum = 0;
+    var i;
+    for(i = 0; i < persArr.length; i++ ){
+        sum += persArr[i].age;
+        arrayForNames[i] = persArr[i].name;
+    };
+        
+    arrayForNames.sort(function (a,b){
+        return a.localeCompare(b, 'sv');
+    });
+    result.names = arrayForNames.join(", ");
+    result.averageAge = Math.round(sum/persArr.length);
+    
+    return result;
+};
