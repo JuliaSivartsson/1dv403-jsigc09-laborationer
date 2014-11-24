1e75df12c2ed00682ba45c770bc9270de730ebdd
juliasivartsson <jsigc09@student.lnu.se>
2014-11-24 16:34:26 +0000

    table with pictures upp

--- 3-gameon/memory/script/memoryGame.js
+++ 3-gameon/memory/script/memoryGame.js
@@ -14,9 +14,30 @@ var Memory = {
                 console.log(number);
       });
       
+              var body = document.getElementsByTagName("body")[0];
+      
+      var tbl = document.createElement("table");
+      var tblBody = document.createElement("tbody");
 
-
+        for(var j = 0; j <= 2; j++){
+            var row = document.createElement("tr");
+            
+            for(var i = 0; i < 4; i++){
+                var cell = document.createElement("td");
+                var imgCell = document.createElement("img");
+                imgCell.setAttribute("src", "pics/0.png");
+                imgCell.setAttribute("alt", "Questionmark");
+                
+                cell.appendChild(imgCell);
+                row.appendChild(cell);
+            }
+            
+            tblBody.appendChild(row);
+        }
+        tbl.appendChild(tblBody);
+        body.appendChild(tbl);
     }
+    
     
 }
 
