5599a0d2ec66daad4ab7d98bdcb2b52d524a4880
juliasivartsson <jsigc09@student.lnu.se>
2014-12-05 16:35:44 +0000

    nextURL IS WORKINGgit add .

--- 4-thequiz/js/script.js
+++ 4-thequiz/js/script.js
@@ -1,19 +1,48 @@
 "use strict";
+window.onload = function(){
+    new Quest();
+};
 
-window.onload = function (){
-  
-  document.getElementById("questionbutton").addEventListener("click", function(){
+
+function Quest(){
+    this.nextURL;
+    this.createHTML();
+    this.init();
+}
+
+Quest.prototype.createHTML = function (){
+    var that = this;
+    
+    var div = document.getElementById("content");
+
+};
+
+Quest.prototype.init = function (){
+    this.getQuestion("http://vhost3.lnu.se:20080/question/1");
+};
+    
+Quest.prototype.getQuestion = function (url){
+    console.log(url);
+    var that = this;
+    
+        var xhr = new XMLHttpRequest();
       
-      var xhr = new XMLHttpRequest();
-      
-    xhr.onreadystatechange = function (){
+        xhr.onreadystatechange = function (){
         
         //Indikerar att vi har fått ett svar
         if(xhr.readyState === 4){
             if(xhr.status == 200){
                 var question = JSON.parse(xhr.responseText);
+                that.popup(question.question);
                 console.log(question.id);
                 console.log(question.message);
+                
+                var button = document.getElementById("answerbutton");
+                button.onclick = function(){
+                    console.log(question.nextURL);
+                    var answer = document.getElementById("svar");
+                  that.sendQuestion(answer.value, question.nextURL);  
+                };
             }
             else{
                 console.log("Läsfel, status:"+xhr.status);
@@ -21,36 +50,48 @@ window.onload = function (){
         }
         
     };
-
-
-      xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
+      xhr.open("GET", url, true);
       xhr.send(null);
-      
-});
-document.getElementById("answerbutton").addEventListener("click", function() {
-    var xhr1 = new XMLHttpRequest();
-    xhr1.onreadystatechange = function (){
-        
-        //Indikerar att vi har fått ett svar
-        if(xhr1.readyState === 4){
-                var answer = JSON.parse(xhr1.responseText);
-                console.log(answer.message);
-
-            if(answer.message == "Correct answer!"){
-                console.log("Rätt svar!");
-            }
-            else{
-                console.log("Fel!");
-            }
-        }
-    };
-    var answer = document.getElementById("svar");
-    var sendAnswer = JSON.stringify({answer: answer.value});
+};
     
-    xhr1.open("POST", "http://vhost3.lnu.se:20080/answer/1", true);
-    xhr1.setRequestHeader('Content-Type', 'application/json');
-    xhr1.send(sendAnswer);
-});
+Quest.prototype.sendQuestion = function (answerInput, url){
+    var that = this;
 
-  
+        var xhr1 = new XMLHttpRequest();
+        
+        xhr1.onreadystatechange = function (){
+            
+            //Indikerar att vi har fått ett svar
+            if(xhr1.readyState === 4){
+            
+            var answer = JSON.parse(xhr1.responseText);
+                    console.log(answer.message);
+    
+                if(answer.message == "Correct answer!"){
+                    that.popup(answer.message);
+                    that.getQuestion(answer.nextURL);             
+                }
+                else{
+                    that.popup(answer.message);
+                }
+            }
+        };
+        
+        //var url = answer.nextURL;
+        var sendAnswer = JSON.stringify({answer: answerInput});
+        console.log(this.nextURL);
+        xhr1.open("POST", url, true);
+        xhr1.setRequestHeader('Content-Type', 'application/json');
+        xhr1.send(sendAnswer);
+};
+
+Quest.prototype.popup = function(text){
+      // Popu meddelande
+      
+        var parentDiv = document.getElementById("content");
+        var div = document.createElement("div");
+        div.className = "popupWindow";
+        parentDiv.appendChild(div);
+        
+        div.innerHTML = text;
 };
