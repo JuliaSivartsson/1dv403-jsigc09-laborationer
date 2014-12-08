906c008fdb55cb4eae18c52425644db030d9d295
juliasivartsson <jsigc09@student.lnu.se>
2014-12-08 17:21:12 +0000

    score is working

--- 4-thequiz/js/script.js
+++ 4-thequiz/js/script.js
@@ -6,19 +6,52 @@ window.onload = function(){
 
 function Quest(){
     this.nextURL;
+    this.questionArray;
     this.init();
 }
 
 Quest.prototype.init = function (){
+    var count = 1;
+    this.questionArray = [];
     document.getElementById("svar").focus();
-    this.getQuestion("http://vhost3.lnu.se:20080/question/1");
+    this.getQuestion("http://vhost3.lnu.se:20080/question/1", count);
 };
+
+Quest.prototype.getScore = function(){
     
-Quest.prototype.getQuestion = function (url){
+    var div = document.getElementById("gameFinished");
+    div.className = "scoreDiv";
+    
+    var awesomeDiv = document.createElement("div");
+    awesomeDiv.className = "awesome";
+
+    
+    var number = 0;
+    var score = 0;
+    var arrayLength = this.questionArray.length;
+    
+        this.questionArray.forEach(function(entry){
+            number++;
+            div.innerHTML += div.innerHTML = "Fråga: "+number+ ", antal gissningar: "+entry.score+"</br>";
+            score += entry.score;
+        });
+        
+    if(score == arrayLength){
+        awesomeDiv.innerHTML = "Du klarade alla frågor på ett försök! Awesome!";
+        div.appendChild(awesomeDiv);
+    }
+    
+    var button = document.getElementById("answerbutton");
+    button.onclick = function(){
+      return false;
+    };
+    
+
+};
+
+Quest.prototype.getQuestion = function (url, count){
     // Här hämtar vi fråga från servern samt skickar nextURL till sendQuestion()
-    console.log(url);
     var that = this;
-    
         var xhr = new XMLHttpRequest();
       
         xhr.onreadystatechange = function (){
@@ -26,24 +59,30 @@ Quest.prototype.getQuestion = function (url){
         //Indikerar att vi har fått ett svar
         if(xhr.readyState === 4){
             if(xhr.status == 200){
-                var question = JSON.parse(xhr.responseText);
-                that.popup(question.question);
-                console.log(question.id);
-                console.log(question.message);
+
+                var div = document.getElementById("question");
+        
+                div.innerHTML = '';
                 
+                var question = JSON.parse(xhr.responseText);
+                that.popup("Fråga: " + question.question);
+                console.log(question.id);
+
+                //that.addQuestion(question.question);
                 var answer = document.getElementById("svar");
                 var button = document.getElementById("answerbutton");
                 answer.onkeypress = function(e){
                     if(e.keyCode == 13 && answer.value !== ""){
                     e.preventDefault();
-                    that.sendQuestion(answer.value, question.nextURL);  
+                    that.sendQuestion(answer.value, question.nextURL, url, count);  
                     }
                 };
                 button.onclick = function(){
                     if(answer.value === ""){
                         return false;
                     }
-                       that.sendQuestion(answer.value, question.nextURL);    
+
+                       that.sendQuestion(answer.value, question.nextURL, url, count);    
                 };
             }
             else{
@@ -56,44 +95,69 @@ Quest.prototype.getQuestion = function (url){
       xhr.send(null);
 };
     
-Quest.prototype.sendQuestion = function (answerInput, url){
+Quest.prototype.sendQuestion = function (answerInput, url, questionUrl, count){
     // Här skickar vi svaret till servern och skickar tillbaka ny fråga till getQuestion()
     var that = this;
-
         var xhr1 = new XMLHttpRequest();
-        
         xhr1.onreadystatechange = function (){
-            
+
             //Indikerar att vi har fått ett svar
             if(xhr1.readyState === 4){
             
             var answer = JSON.parse(xhr1.responseText);
-                    console.log(answer.message);
     
                 if(answer.message == "Correct answer!"){
-                    that.popup(answer.message);
-                    that.getQuestion(answer.nextURL);             
+                    that.saveScore(url, count);
+                    count = 1;
+                    var div = document.getElementById("wrongAnswer");
+                    div.innerHTML = '';
+
+                    if(answer.nextURL !== undefined){
+                    that.getQuestion(answer.nextURL, count);
+                    }
+                    else{
+                        that.popup("Grattis! Du har klarat alla frågor!");
+                        that.getScore();
+                    }
                 }
-                else{
-                    that.popup(answer.message);
+                else if(answer.message === "Wrong answer! :("){
+                    count++;
+
+                    that.popupMessage(answer.message +" Try again!");
+                    that.getQuestion(questionUrl, count);
                 }
             }
         };
         
         var sendAnswer = JSON.stringify({answer: answerInput});
-        console.log(this.nextURL);
         xhr1.open("POST", url, true);
         xhr1.setRequestHeader('Content-Type', 'application/json');
         xhr1.send(sendAnswer);
 };
 
+
+Quest.prototype.saveScore = function(url, score){
+    this.questionArray.push({
+       url: url,
+       score: score
+    });
+    
+};
+
 Quest.prototype.popup = function(text){
       // Popup meddelande
       
-        var parentDiv = document.getElementById("content");
-        var div = document.createElement("div");
+        var div = document.getElementById("question");
         div.className = "popupWindow";
-        parentDiv.appendChild(div);
+        
+        div.innerHTML = text;
+};
+
+Quest.prototype.popupMessage = function(text){
+      // Popup meddelande
+      
+        var div = document.getElementById("wrongAnswer");
+        div.className = "wrongAnswer";
         
         div.innerHTML = text;
 };
